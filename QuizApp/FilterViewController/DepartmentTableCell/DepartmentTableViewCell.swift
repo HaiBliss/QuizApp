6b319cc55ac9cffaa4a8ed0c3619dec5204089a5
haihh <56292447+HaiBliss@users.noreply.github.com>
2022-07-20 21:15:20 +0700

    add filter

---
+++ QuizApp/FilterViewController/DepartmentTableCell/DepartmentTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DepartmentTableViewCell.swift
+//  QuizApp
+//
+//  Created by Hải Vie 🇻🇳 on 20/07/2022.
+//
+
+import UIKit
+
+class DepartmentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ QuizApp/FilterViewController/FilterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FilterViewController.swift
+//  QuizApp
+//
+//  Created by Hải Vie 🇻🇳 on 20/07/2022.
+//
+
+import UIKit
+
+class FilterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
